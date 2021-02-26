e6021555e936c34827c423ef57c80d2236ad55f9
ParadiZzzze <73051260+ParadiZzzze@users.noreply.github.com>
2021-02-26 14:07:47 +0300

    Add files via upload

---
+++ Flight/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.edWeight = new System.Windows.Forms.NumericUpDown();
+            this.label8 = new System.Windows.Forms.Label();
+            this.edSquare = new System.Windows.Forms.NumericUpDown();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btStart = new System.Windows.Forms.Button();
+            this.edSpeed = new System.Windows.Forms.NumericUpDown();
+            this.edAngle = new System.Windows.Forms.NumericUpDown();
+            this.edHeight = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.labDistance = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSquare)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.labDistance);
+            this.panel1.Controls.Add(this.edWeight);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.edSquare);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btStart);
+            this.panel1.Controls.Add(this.edSpeed);
+            this.panel1.Controls.Add(this.edAngle);
+            this.panel1.Controls.Add(this.edHeight);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 165);
+            this.panel1.TabIndex = 0;
+            // 
+            // edWeight
+            // 
+            this.edWeight.Location = new System.Drawing.Point(67, 137);
+            this.edWeight.Name = "edWeight";
+            this.edWeight.Size = new System.Drawing.Size(120, 20);
+            this.edWeight.TabIndex = 15;
+            this.edWeight.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(13, 137);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(44, 13);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Weight:";
+            // 
+            // edSquare
+            // 
+            this.edSquare.Location = new System.Drawing.Point(67, 103);
+            this.edSquare.Name = "edSquare";
+            this.edSquare.Size = new System.Drawing.Size(120, 20);
+            this.edSquare.TabIndex = 13;
+            this.edSquare.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 103);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Square:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(732, 59);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Start";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(732, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(55, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Stop";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(664, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(13, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            // 
+            // btStart
+            // 
+            this.btStart.Location = new System.Drawing.Point(316, 63);
+            this.btStart.Name = "btStart";
+            this.btStart.Size = new System.Drawing.Size(131, 31);
+            this.btStart.TabIndex = 6;
+            this.btStart.Text = "Let\'s go!";
+            this.btStart.UseVisualStyleBackColor = true;
+            this.btStart.Click += new System.EventHandler(this.btStart_Click);
+            // 
+            // edSpeed
+            // 
+            this.edSpeed.Location = new System.Drawing.Point(67, 70);
+            this.edSpeed.Name = "edSpeed";
+            this.edSpeed.Size = new System.Drawing.Size(120, 20);
+            this.edSpeed.TabIndex = 5;
+            this.edSpeed.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // edAngle
+            // 
+            this.edAngle.Location = new System.Drawing.Point(67, 41);
+            this.edAngle.Name = "edAngle";
+            this.edAngle.Size = new System.Drawing.Size(120, 20);
+            this.edAngle.TabIndex = 4;
+            this.edAngle.Value = new decimal(new int[] {
+            45,
+            0,
+            0,
+            0});
+            // 
+            // edHeight
+            // 
+            this.edHeight.Location = new System.Drawing.Point(67, 13);
+            this.edHeight.Name = "edHeight";
+            this.edHeight.Size = new System.Drawing.Size(120, 20);
+            this.edHeight.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Speed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Angle:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Height:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // labDistance
+            // 
+            this.labDistance.AutoSize = true;
+            this.labDistance.Location = new System.Drawing.Point(564, 136);
+            this.labDistance.Name = "labDistance";
+            this.labDistance.Size = new System.Drawing.Size(25, 13);
+            this.labDistance.TabIndex = 16;
+            this.labDistance.Text = "d=0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.edWeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSquare)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edSpeed)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edAngle)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.edHeight)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btStart;
+        private System.Windows.Forms.NumericUpDown edSpeed;
+        private System.Windows.Forms.NumericUpDown edAngle;
+        private System.Windows.Forms.NumericUpDown edHeight;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown edWeight;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown edSquare;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label labDistance;
+    }
+}
+
---
+++ Flight/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double dt = 0.1;
+        double t;
+        BusinessLogic name = new BusinessLogic();
+
+        private void btStart_Click(object sender, EventArgs e)
+        {
+            t = 0;
+            name.Calculating((double)edHeight.Value, (double)edSpeed.Value, (double)edAngle.Value, (double)edSquare.Value, (double)edWeight.Value);
+
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            t += dt;
+            name.finalCountdown(t);
+            label4.Text = "Time:" + Convert.ToString(t);
+            labDistance.Text = "d= " + name.x;
+
+            if (name.y <= 0) timer1.Stop();
+
+        }
+
+        /*Код писался с использованием скелета предыдущей (2ой) лабораторной
+         был удален сам график и поиск "удобного" масштаба, т.к. с точки зрения физики, формулы 
+         были бы неверны. Для расчетов использовались формулы, предоставленные Александром Николаевичем. Насколько помню из школьной программы физики, то
+         при уменьшении массы тела, оно должно лететь дальше. По этим формулам происходит обратное.*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+    }
+
+    public class BusinessLogic
+    {
+        const double g = 9.81;
+        const double C = 0.15;
+        const double rho = 1.29;
+
+        public double x = 0;
+        public double y = 0;
+
+        double t = 0;
+        double y0;
+        double v0;
+        double angle;
+        double S;
+        double m;
+        double vx;
+        double vy;
+        double k;
+        double cosangle;
+        double sinangle;
+
+        public void Calculating(double a,double b, double c, double d, double e)
+        {
+            t = 0;
+            x = 0;
+            y = a;
+            v0 = b;
+            double an = c * Math.PI / 180;
+            S = d;
+            m = e;
+            cosangle = (double)Math.Cos(an);
+            sinangle = (double)Math.Sin(an);            
+            k = 0.5 * C * rho * S / m;
+            vx = v0 * cosangle;
+            vy = v0 * sinangle;
+        }
+
+        public void finalCountdown(double dt)
+        {
+            double v = (double)Math.Sqrt((double)(vx * vx + vy * vy));
+            vx = vx - k * vx * v * dt;
+            vy = vy - (g + k * vy * v) * dt;
+            x = x + vx * dt;
+            y = y + vy * dt;
+
+
+        }
+    }
+}
